a8e4918b31d647644bad35271895e9a21ab8593a
Zameer Ahmed <Zameer.Ahmed@brinks.com>
2026-01-09 20:04:30 +0530

    Update configuration and image assets

--- src/assets/img.tsx
+++ src/assets/img.tsx
@@ -1,8 +1,17 @@
 
+// const getAssetPath = (path: string) => {
+//   const baseUrl = typeof import.meta !== 'undefined' && (import.meta as any).env ? (import.meta as any).env.BASE_URL || '/' : '/AIR_VIEW_TRAVEL/';
+//   return `${baseUrl}${path}`;
+// };
 const getAssetPath = (path: string) => {
-  const baseUrl = typeof import.meta !== 'undefined' && (import.meta as any).env ? (import.meta as any).env.BASE_URL || '/' : '/AIR_VIEW_TRAVEL/';
+  let baseUrl = 'https://airviewtravel.ae';
+  if (window.location.hostname === 'localhost') {
+    baseUrl = 'http://localhost:3000';
+  }
+
   return `${baseUrl}${path}`;
 };
+
 // Carousel Hero Images
 export const carouselImages = {
   travelInsurance: getAssetPath("/images/TravelInsurance.jpg"),//
